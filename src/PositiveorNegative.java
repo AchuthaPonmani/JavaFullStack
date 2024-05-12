b9289a5ac086caa90308a3c29c6b287af992f7a6
AchuthaPonmani <apachutha@gmail.com>
2024-05-12 23:12:49 +0530

    Add files via upload

---
+++ src/PositiveorNegative.java
@@ -0,0 +1,13 @@
+public class PositiveorNegative {
+
+    public static void main(String[] args) {
+        int i=10;
+        if(i>0)
+        {
+            System.out.println(i+ "is a positive number");
+        }
+        else {
+            System.out.println(i+ "is a negative number");
+        }
+    }
+}
